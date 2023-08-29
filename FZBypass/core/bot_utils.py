013d43cd5fd04c114593e6ab1da6e3b63f14b226
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2023-08-29 09:17:42 +0530

    Update bot_utils.py

--- FZBypass/core/bot_utils.py
+++ FZBypass/core/bot_utils.py
@@ -1,21 +1,21 @@
 from pyrogram.filters import create
 from FZBypass import Config
-
-# async def auth_topic(_, __, message):
-#     for chat in Config.AUTH_CHATS:
-#         if ':' in chat:
-#             chat_id, topic_id = chat.split(':')
-#             if (int(chat_id) == message.chat.id 
-#                 and (is_forum := message.reply_to_message)
-#                 and ((is_forum.text is None and int(topic_id) == is_forum.id)
-#                 or (is_forum.text is not None and int(topic_id) == is_forum.reply_to_message_id))):
-#                 return True
-#         elif int(chat) == message.chat.id:
-#             return True
-#     return False
-
-# chat_and_topics = create(auth_topic)
-
+ 
+async def auth_topic(_, __, message):
+    for chat in Config.AUTH_CHATS:
+        if ':' in chat:
+            chat_id, topic_id = chat.split(':')
+            if (int(chat_id) == message.chat.id 
+                and (is_forum := message.reply_to_message)
+                and ((is_forum.text is None and int(topic_id) == is_forum.id)
+                or (is_forum.text is not None and int(topic_id) == is_forum.reply_to_message_id))):
+                return True
+        elif int(chat) == message.chat.id:
+            return True
+    return False
+ 
+chat_and_topics = create(auth_topic)
+ 
 def convert_time(seconds):
     mseconds = seconds * 1000
     periods = [('d', 86400000), ('h', 3600000), ('m', 60000), ('s', 1000), ('ms', 1)]
